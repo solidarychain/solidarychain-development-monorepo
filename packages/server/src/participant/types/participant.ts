aa6ad9a5f104da824d8bef8e10310ac6e666ddc4
Mário Monteiro <mail@koakh.com>
2019-08-14 01:59:44 +0100

    finish controllers

---
+++ packages/server/src/participant/types/participant.ts
@@ -0,0 +1,5 @@
+export interface Participant {
+  name: string;
+  msp: string;
+  identities: any[];
+}
---
+++ packages/server/src/person/types/person.ts
@@ -0,0 +1,7 @@
+// export interface Person {
+//   content: any;
+//   issuedDate: number;
+//   expiresDate: Date;
+//   expired: boolean;
+//   certifierID: string;
+// }
