ec02508465f75e8a73f13b9b704ef3976106c590
Mário Monteiro <mail@koakh.com>
2019-10-03 22:04:53 +0100

    change backup

--- backup.sh
+++ backup.sh
@@ -7,6 +7,7 @@ FILE_EXCLUDE=exclude.tag
 mkdir $DIR -p
 
 touch .bak/$FILE_EXCLUDE
+touch .vscode/chrome/${FILE_EXCLUDE}
 touch node_modules/${FILE_EXCLUDE}
 touch chaincode-person/node_modules/${FILE_EXCLUDE}
 touch packages/common/node_modules/${FILE_EXCLUDE}
@@ -14,6 +15,7 @@ touch packages/participant-cc/node_modules/${FILE_EXCLUDE}
 touch packages/person-cc/node_modules/${FILE_EXCLUDE}
 touch packages/server-graphql/node_modules/${FILE_EXCLUDE}
 touch packages/server-rest/node_modules/${FILE_EXCLUDE}
+touch packages/frontend-react/node_modules/${FILE_EXCLUDE}
 
 tar -zcvf $FILE \
   --exclude-tag-all=$FILE_EXCLUDE \
